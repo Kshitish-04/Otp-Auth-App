86f3506b5ac2aad83cfdf0fd4dd28563163f2e8f
Kshitish 04 <pradhankshitish1@gmail.com>
2025-11-04 18:50:47 +0530

    Updated project

--- Frontend/src/Api.js
+++ Frontend/src/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API = axios.create({baseURL: 'http://localhost:5000/api/auth'})
 const API = axios.create({
-    baseURL: 'https://authentication-app-sage.vercel.app/api/auth',
+    baseURL: 'https://otp-auth-app-phi.vercel.app/',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
